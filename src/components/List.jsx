a97dd2253a42dbee7d23c8538cb6367869492c95
nogi <Createc369.Re@gmail.com>
2023-08-11 18:29:59 +0900

    add: 一覧表示

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,3 +1,17 @@
-export const List = () => {
-  
+export const List = ( {todos} ) => {
+  const complete = () => {
+
+  }
+  return (
+    <div>
+      {todos.map(todo => {
+        return (
+          <div key={todo.id}>
+            <button onClick={() => complete()}>完了</button>
+            <span>{todo.content}</span>
+          </div>
+        )
+      })}
+  </div>
+  )
 }
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,29 @@
 import { List } from "./List"
 import { Form } from "./Form"
+import { useState } from "react";
 
 export const Todo = () => {
-  <>
+  const todosList = () => [
+    {
+      id: 1,
+      content: "店予約する"
+    },
+    {
+      id: 2,
+      content: "卵買う"
+    },
+    {
+      id: 3,
+      content: "郵便出す"
+    },
+  ];
+
+  const [ todos, setTodos ] = useState(todosList);
+  
+  return (
+    <>
     <Form />
-    <List />
-  </>
+    <List todos={todos} />
+    </>
+  )
 }
